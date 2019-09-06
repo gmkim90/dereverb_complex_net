9d59e16f78982175f771baa4956732358ce9dac4
Geonmin Kim <ken.geonmin.kim@gmail.com>
2019-09-07 00:15:59 +0900

    --expnum 369

--- run_train_cloud.sh
+++ run_train_cloud.sh
@@ -6,8 +6,8 @@ arguments=$1
 for ((epoch=0;epoch<=100;epoch++)); do
     echo "training $epoch epoch"
 
-#    python3.7 train.py $arguments --start_epoch $epoch --num_epochs $((epoch+1)) --start_ratio 0.0 --end_ratio 0.25
- #   sleep 5
+    python3.7 train.py $arguments --start_epoch $epoch --num_epochs $((epoch+1)) --start_ratio 0.0 --end_ratio 0.25
+   sleep 5
 
     if [ $epoch == 0 ]; then # git upload after successful execution
 	git checkout cloud
---
+++ run_train_wo_epoch_division.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#arguments=$2
+arguments=$1
+#echo "!!!!!!!!!!!!!! train only 1 epoch for DEBUG !!!!!!!!!!!!!!!!!!!!!"
+for ((epoch=0;epoch<=1000;epoch++)); do
+    echo "training $epoch epoch"
+
+    python3.7 train.py $arguments --start_epoch $epoch --num_epochs $((epoch+1))
+    sleep 5
+
+done
