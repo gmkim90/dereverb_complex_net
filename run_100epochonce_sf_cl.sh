2808a2423e88ee166da471397a462f48be044a75
Geonmin Kim <ken.geonmin.kim@gmail.com>
2019-10-22 12:24:37 +0900

    --expnum 532

---
+++ run_100epochonce_sf_cl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# brain21
+
+arguments=$1
+#for ((epoch=0;epoch<=10000;epoch++)); do
+for epoch in $(seq 0 100 10000); do
+    echo "training $epoch epoch"
+
+    python3.7 train.py $arguments --start_epoch $epoch --num_epochs $((epoch+100)) --start_ratio 0 --end_ratio 1
+    sleep 5
+
+    if [ $epoch == 0 ]; then # git upload after successful execution
+	git checkout cloud_srcfree
+	git add *.py
+	git add *.sh
+	git add models
+        arguments_token=($arguments)
+	git commit -m "${arguments_token[0]} ${arguments_token[1]}"   	
+	git push origin cloud_srcfree
+    fi
+
+done
